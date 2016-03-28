61a4d5150773281e4e70104e0e66c4299609df87
Parth Chandratreya <pac442@bham.ac.uk>
2016-03-28 15:07:02 +0100

    updated loading of form

--- webserver/server.js
+++ webserver/server.js
@@ -3,17 +3,24 @@
  */
 
 var http = require("http");
+var fs = require('fs');
 
 http.createServer(function (request, response) {
-
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
+    response.writeHead(200, {'Content-Type': 'text/plain'}); // Send the HTTP header | HTTP Status: 200 : OK  | Content Type: text/plain
+    displayForm(response)
 }).listen(8081);
 
-// Console will print the message
+
+function displayForm(res) {
+    fs.readFile('form.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        console.log('Loaded form HTML');
+        res.end();
+    });
+}
+
 console.log('Server running at http://127.0.0.1:8081/');
