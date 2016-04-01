f7de81b469cd7fc2cd66e3eef7d8110237b16a63
Parth Chandratreya <pac442@bham.ac.uk>
2016-04-01 22:21:41 +0100

    trying to use cheerio for link parsing

--- webserver/server.js
+++ webserver/server.js
@@ -1,5 +1,5 @@
 /**
- * Created by pac on 26/03/16.
+ * Created by Parth Chandratreya on 26/03/16.
  */
 var http = require('http');
 var fs = require('fs');
@@ -7,13 +7,17 @@ var formidable = require("formidable");
 var util = require('util');
 var url = require("url");
 var request = require('request');
-var HTMLcode = "";
+var cheerio = require('cheerio');
+var website;
+var links;
+var wikiRegex = new RegExp('\/wiki\/[^"]+/g');
 
 var server = http.createServer(function (req, res) {
     if (req.method.toLowerCase() == 'get') {
         displayForm(res);
     } else if (req.method.toLowerCase() == 'post') {
-        urlProcess(req);
+        parseForm(req);
+        parseURL(website);
     }
 });
 
@@ -28,14 +32,23 @@ function displayForm(res) {
     });
 }
 
-function urlProcess(req) {
+function parseForm(req) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields) {
-        request({uri: fields.website}, function (error, response, body) {
-            HTMLcode = body;
-            console.log(HTMLcode);
-        });
+        website = fields.website;
+        console.log(website);
     });
+}
+
+function parseURL(url){
+    console.log(url);
+    // request(url, function(err, resp, body){
+    //     $ = cheerio.load(body);
+    //     links = $('a'); //jquery get all hyperlinks
+    //     $(links).each(function(i, link){
+    //         console.log($(link).text() + ':\n  ' + $(link).attr('href'));
+    //     });
+    // });
 }
 
 server.listen(8081);
